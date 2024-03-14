76e8536ac8bc4e5217227cf1bf534edeec89bdcf
Morales <125131236+MoralesDaxz@users.noreply.github.com>
2024-03-14 16:14:42 +0100

    💄 Social, title, image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Metadata } from "next";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Google",
-  description: "Google clone, built with Next.js and Tailwind CSS",
- 
-}; 
+
 
 export default function RootLayout({
   children,
@@ -20,6 +16,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="https://e7.pngegg.com/pngimages/760/624/png-clipart-google-logo-google-search-advertising-google-company-text.png"></link>
+        <meta property="og:url" content="https://google-opensource-clone.vercel.app/" />
+        <meta property="og:title" content="Google" />
+        <meta property="og:description"
+          content="Buscador, clon de google." />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:image" content="https://e7.pngegg.com/pngimages/760/624/png-clipart-google-logo-google-search-advertising-google-company-text.png" />
+        <title>Google</title>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
